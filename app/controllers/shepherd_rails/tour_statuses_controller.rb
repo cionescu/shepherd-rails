0187c010f15e7e1350cd7f1a6efda6ffc2f3a698
Catalin Ionescu <catalin.ionescu282@gmail.com>
2023-03-15 12:13:05 +0200

    Implement the tour complete callback

--- app/controllers/shepherd_rails/tour_statuses_controller.rb
+++ app/controllers/shepherd_rails/tour_statuses_controller.rb
@@ -1,7 +1,13 @@
 module ShepherdRails
-  class TourStatusesController < ApplicationController
+  class TourStatusesController < ShepherdRails.configuration.base_controller.constantize
     def create
-      puts "Tour statuses"
+      tour_name = params.require(:tour)
+      case params.require(:event)
+      when 'complete'
+        ShepherdRails.configuration.on_tour_complete.call(self, tour_name)
+      end
+
+      head :ok
     end
   end
 end
--- lib/shepherd-rails.rb
+++ lib/shepherd-rails.rb
@@ -1,5 +1,14 @@
 require 'shepherd-rails/version'
 require 'shepherd-rails/engine'
+require 'shepherd-rails/config'
 
 module ShepherdRails
+  class << self
+    attr_accessor :configuration
+
+    def configure
+      self.configuration ||= Config.new
+      yield(configuration)
+    end
+  end
 end
